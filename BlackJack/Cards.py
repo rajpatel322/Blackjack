0cf4b52f47dd01ca53751dfe28b146f94e74bb6b
rajpatel322 <rajkpatel10@gmail.com>
2021-03-04 22:30:22 -0600

    test

---
+++ BlackJack/Cards.py
@@ -0,0 +1,12 @@
+import math
+
+print("cell(4.4) = ", math.ceil(4.4))
+print("floor(4.4) = ", math.floor(4.4))
+
+drink = input("Pick one (coke or pepsi) : ")
+if drink == "coke":
+    print("here is your coke")
+elif drink == "pepsi":
+    print("Here is your pepsi")
+else:
+    print("here is your Water")
